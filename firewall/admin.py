ae1c632d436ae4f670c541388d73e8e97022499e
Őry Máté <orymate@iit.bme.hu>
2013-01-25 11:53:18 +0100

    firewall: ENH admin inlines

--- firewall/admin.py
+++ firewall/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from firewall.models import *
+from django import contrib
+
+
+class AliasInline(contrib.admin.TabularInline):
+    model = Alias
 
 
 class HostAdmin(admin.ModelAdmin):
@@ -8,17 +13,23 @@ class HostAdmin(admin.ModelAdmin):
     list_filter = ('owner', 'vlan', 'groups')
     search_fields = ('hostname', 'description', 'ipv4', 'ipv6', 'mac')
     filter_horizontal = ('groups', 'rules',)
+    inlines = (AliasInline, )
+
+class HostInline(contrib.admin.TabularInline):
+    model = Host
+    fields = ('hostname', 'ipv4', 'ipv6', 'pub_ipv4', 'mac', 'shared_ip', 'owner', 'reverse')
 
 class VlanAdmin(admin.ModelAdmin):
     list_display = ('vid', 'name', 'rules_l', 'ipv4', 'net_ipv4', 'ipv6', 'net_ipv6', 'description', 'domain', 'snat_ip', 'snat_to_l')
     ordering = ('vid',)
+    inlines = (HostInline, )
 
 class RuleAdmin(admin.ModelAdmin):
     list_display = ('r_type', 'color_desc', 'description', 'vlan_l', 'owner', 'extra', 'direction', 'accept', 'proto', 'sport', 'dport', 'nat', 'nat_dport')
     list_filter = ('r_type', 'vlan', 'owner', 'direction', 'accept', 'proto', 'nat')
 
 class AliasAdmin(admin.ModelAdmin):
-        list_display = ('alias', 'host')
+    list_display = ('alias', 'host')
 
 
 admin.site.register(Host, HostAdmin)
