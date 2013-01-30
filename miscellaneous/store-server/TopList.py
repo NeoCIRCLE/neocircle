2802f2b9fac7a5ce85a298494483a5a71c75f073
Őry Máté <orymate@iit.bme.hu>
2013-01-30 12:55:51 +0100

    store: add doc

--- miscellaneous/store-server/TopList.py
+++ miscellaneous/store-server/TopList.py
@@ -7,31 +7,43 @@ COUNT=5
 wm = WatchManager()
 mask = IN_CREATE | IN_MODIFY | IN_DONT_FOLLOW
 
+"""
+Register given file to ~/../.top dir as a symbolic link.
+"""
 def update_new(name):
     if os.path.normpath(name).find("/.") != -1:
         return
     home = pwd.getpwuid(os.stat(name).st_uid).pw_dir
     if not name.startswith(home):
-        return
+        return # outside home
+
     top_dir = os.path.normpath(os.path.join(home, "../.top"))
     try:
         os.mkdir(top_dir)
     except OSError:
         for f in os.listdir(top_dir):
             if os.readlink(os.path.join(top_dir, f)) == name:
-                return
+                return # duplicate
+
     for i in range(1, COUNT):
         try:
             os.rename(os.path.join(top_dir, str(i+1)), os.path.join(top_dir, str(i)))
         except OSError as e:
             pass
+
     os.symlink(name, os.path.join(top_dir, str(COUNT)))
 
+"""
+Process inotify event.
+"""
 class Process(ProcessEvent):
     def process_default(self, event):
         if event.name:
             update_new(os.path.join(event.path, event.name))
 
+"""
+Watch continuously file modifications in /home.
+"""
 def main():
     notifier = Notifier(wm, Process())
     wdd = wm.add_watch('/home', mask, rec=True)
