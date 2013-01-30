6e8292420929a64010be1ba338e3e90656fac290
x <cloud@ik.bme.hu>
2013-01-30 22:36:39 +0100

    dump: exclude some more apps

--- miscellaneous/dump.sh
+++ miscellaneous/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-/opt/webadmin/cloud/manage.py dumpdata -e admin -e auth.permission -e contenttypes -e sessions -e djcelery --format=json --indent=2|grep -v '"password":'|sed -e 's/^.*"smb_password":.*$/"smb_password": "kamu",/' -e 's/BEGIN RSA PRIVATE.*END RSA/xxx/' >/opt/webadmin/cloud/miscellaneous/dump.json
+/opt/webadmin/cloud/manage.py dumpdata -e admin -e one.userclouddetails -e auth.permission -e contenttypes -e sessions -e djcelery --format=json --indent=2|grep -v '"password":'|sed -e 's/^.*"smb_password":.*$/"smb_password": "kamu",/' -e 's/BEGIN RSA PRIVATE.*END RSA/xxx/' >/opt/webadmin/cloud/miscellaneous/dump.json
 
 
