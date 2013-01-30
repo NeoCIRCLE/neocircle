816a72f182bafc5e0851547e9e072d1a4782f7e3
x <cloud@ik.bme.hu>
2013-01-30 19:58:58 +0100

    firewall: random fix

--- firewall/views.py
+++ firewall/views.py
@@ -17,8 +17,6 @@ import sys
 def reload_firewall(request):
     if request.user.is_authenticated():
         if(request.user.is_superuser):
-            ipv4 = firewall()
-            return HttpResponse(ipv4.show())
             html = u"Be vagy jelentkezve es admin is vagy, kedves %s!" % request.user.username
             html += "<br> 10 masodperc mulva ujratoltodik"
             ReloadTask.delay()
