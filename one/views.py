f57e25ec4ca1b387e0bc072d9e73c4bee98ff4a6
Dányi Bence <madbence@gmail.com>
2013-02-08 15:13:27 +0100

    one: quote style fixed

--- one/views.py
+++ one/views.py
@@ -70,7 +70,7 @@ def _list_instances(request):
 @login_required
 def home(request):
     return render_to_response("home.html", RequestContext(request,{
-	"templates": Template.objects.all(),
+        'templates': Template.objects.all(),
         'instances': _list_instances(request),
         }))
 
