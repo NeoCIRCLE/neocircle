2b501ced12085f64f08281210510aff76cd34168
Őry Máté <orymate@iit.bme.hu>
2013-02-13 17:39:30 +0100

    one: add full names to admin

--- one/admin.py
+++ one/admin.py
@@ -5,6 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 from one import models
 import string
 
+def owner_person(obj):
+    p = obj.owner
+    return "%s %s (%s)" % (p.last_name, p.first_name, p.username)
+owner_person.short_description = _('owner')
+
 class PersonInline(contrib.admin.StackedInline):
     model = models.Person
     max_num = 1
@@ -47,13 +52,13 @@ submit_vm.short_description = _('Submit VM')
 
 class TemplateAdmin(contrib.admin.ModelAdmin):
     model=models.Template
-    list_display = ('name', 'state', 'owner', 'system', 'public')
+    list_display = ('name', 'state', owner_person, 'system', 'public')
     list_filter = ('owner', 'public')
 
 class InstanceAdmin(contrib.admin.ModelAdmin):
     model=models.Instance
     actions = [update_state, submit_vm]
-    list_display = ('id', 'name', 'owner', 'state')
+    list_display = ('id', 'name', owner_person, 'state')
     readonly_fields = ('ip', 'active_since', 'pw', 'template')
     list_filter = ('owner', 'template', 'state')
 
@@ -64,7 +69,7 @@ class NetworkAdmin(contrib.admin.ModelAdmin):
 class ShareAdmin(contrib.admin.ModelAdmin):
     model=models.Network
     list_filter = ('group', 'template', )
-    list_display = ('name', 'owner', 'template', 'group', )
+    list_display = ('name', owner_person, 'template', 'group', )
 
 contrib.admin.site.register(models.Template, TemplateAdmin)
 contrib.admin.site.register(models.Instance, InstanceAdmin)
