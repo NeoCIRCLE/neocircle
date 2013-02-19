59ca3ce37f0c3fdab0666cb546e9a9c974066ac6
Dányi Bence <madbence@gmail.com>
2013-02-19 16:11:36 +0100

    webui: csrf token handling moved to util.js

--- one/static/script/util.js
+++ one/static/script/util.js
@@ -1,4 +1,33 @@
 var cloud = (function(cloud) {
+
+    function getCookie(name) {
+        var cookieValue = null;
+        if(document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for(var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if(cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        return(/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        crossDomain: false,
+        beforeSend: function(xhr, settings) {
+            if(!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
     /**
      * Convert bytes to human readable format
      */
