b7f905d1f944f490817c02f779910e2d2e7b1481
Dudás Ádám <sir.dudas.adam@gmail.com>
2013-03-07 11:48:16 +0100

    one: test for the index

--- one/tests.py
+++ one/tests.py
@@ -1,16 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ViewsTestCase(TestCase):
+    def test_index(self):
+        '''Test whether index is reachable.'''
+        resp = self.client.get('/', follow=True)
+        self.assertEqual(resp.status_code, 200)
