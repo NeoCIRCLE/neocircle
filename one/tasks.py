d121ff942f5441c383e2e34023f9a274cb1331e1
Őry Máté <orymate@iit.bme.hu>
2013-03-10 09:40:54 +0100

    one: send_mail optional sender

--- one/tasks.py
+++ one/tasks.py
@@ -9,8 +9,6 @@ from django.core.mail import send_mail
 logger = logging.getLogger(__name__)
 
 class SendMailTask(Task):
-    def run(self, to, subject, msg):
-        sender = u'noreply@cloud.ik.bme.hu'
-
+    def run(self, to, subject, msg, sender=u'noreply@cloud.ik.bme.hu'):
         send_mail(subject, msg, sender, [ to ], fail_silently=False)
         logger.info("[django][one][tasks.py] %s->%s [%s]" % (sender, to, subject) )
