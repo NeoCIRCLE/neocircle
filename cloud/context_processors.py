b258b5a90c489c6b3247de8863615fd0c2b8182b
Bence Dányi <madbence@gmail.com>
2013-03-14 14:08:56 +0100

    cloud: stat debug data added

--- cloud/context_processors.py
+++ cloud/context_processors.py
@@ -7,7 +7,21 @@ def process_debug(req):
     return {'DEBUG': DEBUG}
 
 def process_stat(req):
-    stat = cache.get('cloud_stat')
+    if STAT_DEBUG:
+        stat = {
+            'CPU': {
+                'USED_CPU': 10,
+                'ALLOC_CPU': 20,
+                'FREE_CPU': 70
+            },
+            'MEM': {
+                'USED_MEM': 567,
+                'ALLOC_MEM': 371,
+                'FREE_MEM': 2048-567-371
+            }
+        }
+    else:
+        stat = cache.get('cloud_stat')
     return {
         'STAT_DEBUG': STAT_DEBUG,
         'cloud_stat': stat,
