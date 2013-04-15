a2394d564b0fe8e93cfa34ded2eaea65a3f502b8
Őry Máté <orymate@iit.bme.hu>
2013-04-15 15:33:18 +0200

    django: better devenv

--- cloud/settings/dev.py
+++ cloud/settings/dev.py
@@ -6,3 +6,12 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+ADMINS = (
+        ('Ory, Mate', 'orymate@localhost'),
+        )
+MANAGERS = (
+        ('Ory Mate', 'maat@localhost'),
+        )
+INSTALLED_APPS += ("debug_toolbar", )
+MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
