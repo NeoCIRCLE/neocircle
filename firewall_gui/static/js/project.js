4076273310171f9caed22dd52a39e596fc2aebd6
Bence Dányi <madbence@gmail.com>
2013-04-24 13:30:37 +0200

    firewall_gui: js rulelistcontroller added

--- firewall_gui/static/js/project.js
+++ firewall_gui/static/js/project.js
@@ -1,1 +1,16 @@
-/* Project specific Javascript goes here. */
+angular.module('firewall', []).config(
+['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/rules/', {
+        templateUrl: '/static/partials/rule-list.html',
+        controller: RuleListCtrl
+    }).
+    otherwise({
+        redirectTo: '/rules/'
+    });
+}]);
+
+function RuleListCtrl($scope, $http) {
+    $http.get('/firewall/rules/').success(function success(data) {
+        $scope.rules = data;
+    });
+}
