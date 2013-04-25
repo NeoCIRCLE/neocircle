a7dbda8c985ec9f3fc4b76fa5cc796e4d9b5ab25
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-04-25 13:50:52 +0200

    laborgui: Added store_host variable to the settings.

--- cloud/settings/prod.py
+++ cloud/settings/prod.py
@@ -6,3 +6,4 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 STORE_SETTINGS['store_url'] = 'https://store.cloud.ik.bme.hu'
 STORE_SETTINGS['ssl_auth'] = 'True'
+STORE_SETTINGS['store_host'] = 'store.cloud.ik.bme.hu'
