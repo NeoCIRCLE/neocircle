7b3586251731906fd226e26d30c4ee8dee86c188
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-05-07 15:56:37 +0200

    laborgui: updated version to 0.3

--- miscellaneous/laborclient/setup.py
+++ miscellaneous/laborclient/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "CloudGUI",
-    version = "0.2",
+    version = "0.3",
     packages = ['cloudgui',], 
     scripts = ['cloud','rdp',],
     )
