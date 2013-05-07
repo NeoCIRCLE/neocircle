21bb630b5b86e203d0b24e8008c72ea805e4e182
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-05-07 18:32:18 +0200

    laborgui: Added desktop and icon to install package

--- miscellaneous/laborclient/setup.py
+++ miscellaneous/laborclient/setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name = "CloudGUI",
     version = "0.3",
+    data_files = [('/usr/share/icons', ['cloud.svg']),
+                    ('/usr/share/applications', ['cloud.desktop'])],
+    include_package_data = True,
     packages = ['cloudgui',], 
     scripts = ['cloud','rdp',],
     )
