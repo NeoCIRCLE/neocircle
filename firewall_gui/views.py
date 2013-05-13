6f6485e4b5efb54415c4b97da6eaff246fcf2218
Bence Dányi <madbence@gmail.com>
2013-05-13 14:36:06 +0200

    firewall_gui: code refactored

--- firewall_gui/views.py
+++ firewall_gui/views.py
@@ -82,51 +82,20 @@ def list_entities(request, name):
             'nat',
             'accept',
             'description']),
+        'hosts': make_entity_lister(Host, [
+            'id',
+            'reverse',
+            'ipv4',
+            'shared_ip',
+            'description',
+            'comment',
+            'location',
+            'vlan',
+            'owner',
+            'created_at',
+            'modified_at',
+            'groups']),
         }[name](request)), content_type='application/json')
-
-def list_rules(request):
-    rules = [{
-        'id': rule.id,
-        'target': {
-            'name': rule.vlan.name,
-            'id': rule.vlan.id,
-            'type': 'vlan',
-        } if rule.vlan else {
-            'name': rule.vlangroup.name,
-            'id': rule.vlangroup.id,
-            'type': 'vlangroup',
-        } if rule.vlangroup else {
-            'name': rule.hostgroup.name,
-            'id': rule.hostgroup.id,
-            'type': 'hostgroup',
-        } if rule.hostgroup else {
-            'name': rule.firewall.name,
-            'id': rule.firewall.id,
-            'type': 'firewall',
-        } if rule.firewall else {
-            'name': rule.host.hostname,
-            'id': rule.host.id,
-            'type': 'host',
-        },
-        'type': rule.r_type,
-        'direction': rule.get_direction_display(),
-        'proto': rule.proto,
-        'owner': {
-            'name': str(rule.owner),
-            'id': rule.owner.id
-        },
-        'foreignNetwork': {
-            'name': rule.foreign_network.name,
-            'id': rule.foreign_network.id,
-        },
-        'created_at': rule.created_at.isoformat(),
-        'modified_at': rule.modified_at.isoformat(),
-        'nat': rule.nat,
-        'accept': rule.accept,
-        'description': rule.description,
-    } for rule in Rule.objects.all()]
-    return HttpResponse(json.dumps(rules), content_type='application/json')
-
 
 def list_hosts(request):
     hosts = [{
