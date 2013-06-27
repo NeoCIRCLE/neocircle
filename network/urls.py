68da196dffcd836131b9df4c2ef9a49c93732f5b
Őry Máté <orymate@iit.bme.hu>
2013-06-27 21:10:33 +0200

    network: add index view

--- network/urls.py
+++ network/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
+from .views import IndexView, HostList, HostDetail
+
+
+urlpatterns = patterns(
+    '',
+    url('^$', IndexView.as_view(), name='network.index'),
 )
