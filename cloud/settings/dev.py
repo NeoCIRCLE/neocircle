c7ed5d681e5b4557619368f52779f33f678c567d
Őry Máté <orymate@iit.bme.hu>
2013-06-27 15:59:45 +0200

    dev: disable crispy silent fail

--- cloud/settings/dev.py
+++ cloud/settings/dev.py
@@ -15,3 +15,6 @@ MANAGERS = (
         )
 INSTALLED_APPS += ("debug_toolbar", )
 MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+INTERNAL_IPS = [('2001:738:2001:4031:5:253:%d:0' % i) for i in xrange(1, 100)]
+INTERNAL_IPS += [('10.5.253.%d' % i) for i in xrange(1, 100)]
+CRISPY_FAIL_SILENTLY = False
