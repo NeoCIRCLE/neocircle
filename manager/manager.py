db3ffeb2a99b9382b2aa9b6dd94367945862e5e0
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-09-03 10:48:29 +0200

    local celery manager for tracking tasks

--- manager/manager.py
+++ manager/manager.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 
 from mancelery import celery
+from celery import current_task
+
+import scheduler
 
 
 @celery.task
-def create_vm():
+def deploy(instance):
     '''Create new virtual machine from VM class.
     '''
-    # Get info from scheduler (free sapce, node with enough cpu and ram)
+    # Get info from scheduler (free space, node with enough cpu and ram)
+    current_task.update_state(state='PENDING')
+    instance.node = scheduler.get_node()
 
     # Create hard drives (storage)
+    current_task.update_state(state='PREPARE')
+    for disk in instance.disks:
+        disk.deploy()
+
+    # Create context
+    instance.create_context()
 
     # Create machine (vmdriver)
+    current_task.update_state(state='DEPLOY VM')
+    instance.deploy_task()
+
     # Estabilish network connection (vmdriver)
+    current_task.update_state(state='DEPLOY NET')
+    instance.deploy_net()
     # Resume machine (vmdriver)
+    current_task.update_state(state='BOOT')
+    instance.resume()
     pass
 
 
@@ -38,4 +56,8 @@ def restart():
 
 
 def reset():
+    pass
+
+
+def migrate():
     pass
