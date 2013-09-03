d40a8aad3555dcfa34b3b95afcaf30252f8d977b
Őry Máté <ory.mate@cloud.bme.hu>
2013-09-03 12:47:39 +0200

    initial commit of new project structure

---
+++ README.rst
@@ -0,0 +1,5 @@
+=============
+cirecle-cloud
+=============
+
+This is the Django based controller and web portal of the CIRCLE Cloud.
---
+++ circle/circle/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #url(r'^$', TemplateView.as_view(template_name='base.html')),
+
+    # Examples:
+    # url(r'^$', 'circle.views.home', name='home'),
+    # url(r'^circle/', include('circle.foo.urls')),
+
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
