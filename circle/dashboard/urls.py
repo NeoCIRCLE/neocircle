743f544fe3b8469c1aa04ec4979a81b69f682f74
Bach Dániel <bd@ik.bme.hu>
2013-09-04 16:52:10 +0200

    dashboard: add initial novnc console

--- circle/dashboard/urls.py
+++ circle/dashboard/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 
 from .views import IndexView, VmDetailView
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', IndexView.as_view()),
-    url(r'^vm/\d+/$', VmDetailView.as_view()),
+urlpatterns = patterns('',
+                       url(r'^$', IndexView.as_view()),
+                       url(r'^vm/(?P<id>\d+)/$', VmDetailView.as_view(),
+                           name='dashboard.views.detail'),
 )
