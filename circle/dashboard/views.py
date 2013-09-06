9a7736ad7381523a05f80a5e56fd31627d349333
Őry Máté <ory.mate@cloud.bme.hu>
2013-09-06 13:03:38 +0200

    dashboard: use instance data from models
    First try to use dynamic model data instead of dummy static html.

--- circle/dashboard/views.py
+++ circle/dashboard/views.py
@@ -1,8 +1,16 @@
 from django.views.generic import TemplateView
+from vm.models import Instance
 
 
 class IndexView(TemplateView):
     template_name = "dashboard/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            'instances': Instance.objects.filter(owner=self.request.user),
+        })
+        return context
 
 
 class VmDetailView(TemplateView):
