f6fb21d837c576aa247e7a9cfc9b021547f8eb44
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-09-09 15:33:01 +0200

    adding simple debug scheduler

--- circle/manager/scheduler.py
+++ circle/manager/scheduler.py
@@ -1,2 +1,11 @@
-def get_node():
-    pass
+from vm.models import Node
+
+def get_node(instance):
+    ''' Select a node for an hosting an instance
+    based on requirements.
+    '''
+    # Return first Node or None
+    try:
+        return Node.objects.all()[0]
+    except:
+        return None
