328afa24af01b70564428533e9cf1a69d108d7d1
Bach Dániel <bd@doszgep.hu>
2013-09-09 16:20:25 +0200

    manager: fix ImportError in scheduler.py

--- circle/manager/scheduler.py
+++ circle/manager/scheduler.py
@@ -1,10 +1,9 @@
-from vm.models import Node
-
 def get_node(instance):
     ''' Select a node for an hosting an instance
     based on requirements.
     '''
     # Return first Node or None
+    from vm.models import Node
     try:
         return Node.objects.all()[0]
     except:
