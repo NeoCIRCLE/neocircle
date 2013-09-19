ca16a38083d7a7a800228f45dfd6cb92d974f2c9
Guba Sándor <guba.sandor@cloud.bme.hu>
2013-09-19 18:01:09 +0200

    vm: generating stubs

--- circle/vm/tasks.py
+++ circle/vm/tasks.py
@@ -2,5 +2,55 @@ from manager.mancelery import celery
 
 
 @celery.task(name='vmdriver.create')
-def create(parameters):
+def create(params):
+    pass
+
+
+@celery.task(name='vmdriver.suspend')
+def stop(params):
+    pass
+
+
+@celery.task(name='vmdriver.resume')
+def resume(params):
+    pass
+
+
+@celery.task(name='vmdriver.delete')
+def poweroff(params):
+    pass
+
+
+@celery.task(name='vmdriver.shutdown')
+def shutdown(params):
+    pass
+
+
+@celery.task(name='vmdriver.reset')
+def reset(params):
+    pass
+
+
+@celery.task(name='vmdriver.start')
+def restart(params):
+    pass
+
+
+@celery.task(name='vmdriver.reboot')
+def reboot(params):
+    pass
+
+
+@celery.task(name='vmdriver.save')
+def save(params):
+    pass
+
+
+@celery.task(name='vmdriver.restore')
+def restore(params):
+    pass
+
+
+@celery.task(name='vmdriver.migrate')
+def migrate(params):
     pass
