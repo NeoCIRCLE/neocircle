c0ad4f0b1b6eb52ee57546a0ddda2f0d8069cbce
Dudás Ádám <dudas.adam@cloud.ik.bme.hu>
2013-09-27 14:53:43 +0200

    vm: add Interface model to admin
    Also sort importation and registration of models by name.

--- circle/vm/admin.py
+++ circle/vm/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import (Node, InstanceTemplate, InterfaceTemplate, Instance,
-                     InstanceActivity, NodeActivity, Lease)
+from .models import (Instance, InstanceActivity, InstanceTemplate, Interface,
+                     InterfaceTemplate, Lease, Node, NodeActivity)
 
 
-admin.site.register(Node)
-admin.site.register(InstanceTemplate)
-admin.site.register(InterfaceTemplate)
 admin.site.register(Instance)
 admin.site.register(InstanceActivity)
-admin.site.register(NodeActivity)
+admin.site.register(InstanceTemplate)
+admin.site.register(Interface)
+admin.site.register(InterfaceTemplate)
 admin.site.register(Lease)
+admin.site.register(Node)
+admin.site.register(NodeActivity)
