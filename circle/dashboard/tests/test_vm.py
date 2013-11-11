045396f9c904e06a731aa0907f699fe9f0160a85
Őry Máté <ory.mate@cloud.bme.hu>
2013-11-11 11:12:39 +0100

    dashboard: add basic tests

---
+++ circle/dashboard/tests/test_vm.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User, Group
+
+from vm.models import Instance
+
+
+
+class VmDetailTest(TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create(username='user1')
+        self.u2 = User.objects.create(username='user2', is_staff=True)
+        self.us = User.objects.create(username='superuser', is_superuser=True)
+        self.g1 = Group.objects.create(name='group1')
+        self.g1.user_set.add(self.u1)
+        self.g1.user_set.add(self.u2)
+        self.g1.save()
+
+    def test_404_vm_page(self):
+        c = Client()
+        response = c.get('/dashboard/vm/235555/')
+        self.assertEqual(response.status_code, 404)
