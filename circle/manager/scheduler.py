c5b3cd6aa4b6f1c08da5ae6ffc738d080f4a38cb
Dudás Ádám <dudas.adam@cloud.ik.bme.hu>
2013-11-15 14:48:32 +0100

    manager: add trait checking to scheduler

--- circle/manager/scheduler.py
+++ circle/manager/scheduler.py
@@ -3,6 +3,8 @@ def get_node(instance, nodes):
     '''
     # Return first Node or None
     try:
+        req_traits = set(instance.req_traits.all())
+        nodes = [n for n in nodes if req_traits.issubset(n.traits.all())]
         return nodes[0]
     except:
         return None
