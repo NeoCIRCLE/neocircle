6d5b73639451e571d6fb16e9f56fcc4fb9fb41d8
Őry Máté <ory.mate@cloud.bme.hu>
2014-02-05 21:48:45 +0100

    dashboard: add Profile model
    Closes #43

--- circle/dashboard/models.py
+++ circle/dashboard/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models import Model, ForeignKey
+from django.db.models import Model, ForeignKey, OneToOneField, CharField
+from django.utils.translation import ugettext_lazy as _
 
 from vm.models import Instance
 
@@ -7,3 +9,14 @@ from vm.models import Instance
 class Favourite(Model):
     instance = ForeignKey(Instance)
     user = ForeignKey(User)
+
+
+class Profile(Model):
+    user = OneToOneField(User)
+    preferred_language = CharField(verbose_name=_('preferred language'),
+                                   choices=settings.LANGUAGES,
+                                   max_length=32,
+                                   default=settings.LANGUAGE_CODE, blank=False)
+    org_id = CharField(  # may be populated from eduPersonOrgId field
+        unique=True, blank=True, null=True, max_length=64,
+        help_text=_('Unique identifier of the person, e.g. a student number.'))
