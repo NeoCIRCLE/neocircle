c6f318d9df17b9aa8c3b85dec2b24b067ffda9c9
Őry Máté <ory.mate@cloud.bme.hu>
2014-02-06 11:17:27 +0100

    dashboard: add logging to model

--- circle/dashboard/models.py
+++ circle/dashboard/models.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db.models import Model, ForeignKey, OneToOneField, CharField
@@ -5,6 +7,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from vm.models import Instance
 
+
+logger = getLogger(__name__)
 
 class Favourite(Model):
     instance = ForeignKey(Instance)
@@ -23,15 +27,25 @@ class Profile(Model):
 
 
 if hasattr(settings, 'SAML_ORG_ID_ATTRIBUTE'):
+    logger.debug("Register save_org_id to djangosaml2 pre_user_save")
     from djangosaml2.signals import pre_user_save
 
     def save_org_id(sender, attributes, user_modified):
+        logger.debug("save_org_id called by %s", sender.username)
         atr = settings.SAML_ORG_ID_ATTRIBUTE
         value = attributes[atr]
         profile, created = Profile.objects.get_or_create(user=sender)
         if created or profile.org_id != value:
+            logger.info("org_id of %s added to user %s's profile",
+                        value, sender.username)
             profile.org_id = value
             profile.save()
+        else:
+            logger.debug("org_id of %s already added to user %s's profile",
+                         value, sender.username)
         return False
 
+
     pre_user_save.connect(save_org_id, weak=False)
+else:
+    logger.debug("Do not register save_org_id to djangosaml2 pre_user_save")
