7d96e460240aa0ae0b7939cb760e99fa1820af72
Őry Máté <ory.mate@cloud.bme.hu>
2014-02-09 14:18:44 +0100

    vm: remove InstanceTestCase

--- circle/vm/tests/test_models.py
+++ circle/vm/tests/test_models.py
@@ -24,43 +24,6 @@ class TemplateTestCase(TestCase):
         # TODO add images & net
 
 
-class InstanceTestCase(TestCase):
-
-    def test_pre_state_changed_w_exception(self):
-        """Signal handler of pre_state_changed prevents save with Exception."""
-        def callback(sender, new_state, **kwargs):
-            if new_state == 'invalid value':
-                raise Exception()
-        pre_state_changed.connect(callback)
-        i = Instance(state='NOSTATE')
-        i.save = Mock()
-        i.state_changed('invalid value')
-        assert i.state == 'NOSTATE'
-        assert not i.save.called
-
-#    def test_pre_state_changed_wo_exception(self):
-#        """Signal handler of pre_state_changed allows save."""
-#        mock = Mock()
-#        pre_state_changed.connect(mock)
-#        i = Instance(state='NOSTATE')
-#        i.save = Mock()
-#        i.state_changed('RUNNING')
-#        assert i.state == 'RUNNING'
-#        assert mock.called
-#        assert i.save.called
-
-#    def test_post_state_changed(self):
-#        """Signal handler of post_state_changed runs."""
-#        mock = Mock()
-#        post_state_changed.connect(mock)
-#        i = Instance(state='NOSTATE')
-#        i.save = Mock()
-#        i.state_changed('RUNNING')
-#        assert mock.called
-#        assert i.save.called
-#        assert i.state == 'RUNNING'
-
-
 class InterfaceTestCase(TestCase):
 
     def test_interface_create(self):
