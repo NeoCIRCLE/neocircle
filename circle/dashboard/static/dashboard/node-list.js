2664addcd74288239e4c5cc9491b876e16315a76
Oláh István Gergely <olah.istvan.gergely@cloud.bme.hu>
2014-03-11 22:27:12 +0100

    dashboard: traits quickfix for merging

--- circle/dashboard/static/dashboard/node-list.js
+++ circle/dashboard/static/dashboard/node-list.js
@@ -196,30 +196,6 @@ $(function() {
   }
 
 
-  // remove trait
-  $('.node-details-remove-trait').click(function() {
-    var to_remove =  $(this).data("trait-pk");
-    var clicked = $(this);
-    $.ajax({
-      type: 'POST',
-      url: location.href,
-      headers: {"X-CSRFToken": getCookie('csrftoken')}, 
-      data: {'to_remove': to_remove},
-      success: function(re) {
-        if(re['message'].toLowerCase() == "success") {
-          $(clicked).closest(".label").fadeOut(500, function() {
-            $(this).remove();
-          });
-        }
-      },
-      error: function() {
-        addMessage(re['message'], 'danger');
-      }
-
-    });
-    return false;
-  });
-
 
 // refresh the given contents, parameter is the array of contents, in pair
   function contentrefresh(elements,callbacks){
