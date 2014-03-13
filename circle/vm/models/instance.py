NeoCIRCLE/neocircle

commit f70812a375618879c536bd90a86a3122073f3643

vm: extract generic functionality
vm: refactor find_unused_port function

circle/vm/models/instance.py
@@ -40,12 +40,26 @@ ACCESS_METHODS = [(key, name) for key, (name, port, transport)
 VNC_PORT_RANGE = (2000, 65536)  # inclusive start, exclusive end
 
 
+def find_unused_port(port_range, used_ports=[]):
+    """Find an unused port in the specified range.
+
+    The list of used ports can be specified optionally.
+
+    :param port_range: a tuple representing a port range (w/ exclusive end)
+                       e.g. (6000, 7000) represents ports 6000 through 6999
+    """
+    ports = xrange(*port_range)
+    used = set(used_ports)
+    unused = (port for port in ports if port not in used)
+    return next(unused, None)  # first or None
+
+
 def find_unused_vnc_port():
-    used = set(Instance.objects.values_list('vnc_port', flat=True))
-    for p in xrange(*VNC_PORT_RANGE):
-        if p not in used:
-            return p
-    else:
+    port = find_unused_port(
+        port_range=VNC_PORT_RANGE,
+        used_ports=Instance.objects.values_list('vnc_port', flat=True))
+
+    if port is None:
         raise Exception("No unused port could be found for VNC.")
 
 

commit ea9874e1ef46d72b9491ec4126d52a80cf3f62c8

vm: add missing return statement to find_unused_vnc_port
bug introduced in f70812a3

circle/vm/models/instance.py
@@ -61,6 +61,8 @@ def find_unused_vnc_port():
 
     if port is None:
         raise Exception("No unused port could be found for VNC.")
+    else:
+        return port
 
 
 class InstanceActiveManager(Manager):