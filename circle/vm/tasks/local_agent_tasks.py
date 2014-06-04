1c5588304142cab0e78b7364aced9f9b3b8a7da1
Bach Dániel <bd@ik.bme.hu>
2014-06-04 14:09:09 +0200

    vm: refactor agent tasks

--- circle/vm/tasks/local_agent_tasks.py
+++ circle/vm/tasks/local_agent_tasks.py
@@ -21,23 +21,32 @@ from vm.tasks.agent_tasks import (restart_networking, change_password,
 import time
 
 
+def send_init_commands(instance, act, vm):
+    queue = instance.get_remote_queue_name("agent")
+
+    with act.sub_activity('restart_networking'):
+        restart_networking.apply_async(queue=queue, args=(vm, ))
+    with act.sub_activity('change_password'):
+        change_password.apply_async(queue=queue, args=(vm, instance.pw))
+    with act.sub_activity('set_time'):
+        set_time.apply_async(queue=queue, args=(vm, time.time()))
+    with act.sub_activity('set_hostname'):
+        set_hostname.apply_async(
+            queue=queue, args=(vm, instance.primary_host.hostname))
+
+
 @celery.task
 def agent_started(vm):
-    from vm.models import Instance, instance_activity
+    from vm.models import Instance, instance_activity, InstanceActivity
     instance = Instance.objects.get(id=int(vm.split('-')[-1]))
+    initialized = InstanceActivity.objects.filter(
+        instance=instance, activity_code='vm.Instance.agent').count()
 
     with instance_activity(code_suffix='agent', instance=instance) as act:
         with act.sub_activity('starting'):
-            queue = "%s.agent" % instance.node.host.hostname
-            print queue
-            restart_networking.apply_async(queue=queue,
-                                           args=(vm, ))
-            change_password.apply_async(queue=queue,
-                                        args=(vm, instance.pw))
-            set_time.apply_async(queue=queue,
-                                 args=(vm, time.time()))
-            set_hostname.apply_async(queue=queue,
-                                     args=(vm, instance.primary_host.hostname))
+            pass
+        if not initialized:
+            send_init_commands(instance, act, vm)
 
 
 @celery.task
@@ -48,15 +57,4 @@ def agent_stopped(vm):
                                          activity_code='vm.Instance.agent')
     act = qs.latest('id')
     with act.sub_activity('stopping'):
-        pass
-
-
-@celery.task
-def agent_ok(vm):
-    from vm.models import Instance, InstanceActivity
-    instance = Instance.objects.get(id=int(vm.split('-')[-1]))
-    qs = InstanceActivity.objects.filter(instance=instance,
-                                         activity_code='vm.Instance.agent')
-    act = qs.latest('id')
-    with act.sub_activity('ok'):
         pass
