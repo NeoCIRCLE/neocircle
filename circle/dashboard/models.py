e67649226a2994f6b874055d66440e1a47a7ac1c
Kálmán Viktor <kviktor@cloud.bme.hu>
2014-06-04 16:06:49 +0200

    dashboard: update profile absolute url

--- circle/dashboard/models.py
+++ circle/dashboard/models.py
@@ -90,7 +90,7 @@ class Profile(Model):
                                  valid_until)
 
     def get_absolute_url(self):
-        return reverse("dashboard.views.profile")
+        return reverse("dashboard.views.profile", kwargs={'pk': self.user.pk})
 
 
 class GroupProfile(AclBase):
