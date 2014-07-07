22bc8ba0202c490b2faffbd2b9b6f4bf9e9cf76f
Bach Dániel <bd@ik.bme.hu>
2014-07-07 14:19:32 +0200

    storage: fix flake8 warnings

--- circle/storage/tasks/storage_tasks.py
+++ circle/storage/tasks/storage_tasks.py
@@ -72,9 +72,11 @@ def make_free_space(datastore, percent):
 def move_to_trash(datastore, disk_path):
     pass
 
+
 @celery.task(name='storagedriver.recover_from_trash')
 def recover_from_trash(datastore, disk_path):
     pass
+
 
 @celery.task(name='storagedriver.get_storage_stat')
 def get_storage_stat(path):
