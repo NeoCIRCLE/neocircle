5f4f2015a1ab66f4ca59f84b3c6636e109bbb3cf
Kálmán Viktor <kviktor@cloud.bme.hu>
2014-07-11 11:39:05 +0200

    dashboard: fix js search

--- circle/dashboard/static/dashboard/dashboard.js
+++ circle/dashboard/static/dashboard/dashboard.js
@@ -381,7 +381,7 @@ function generateVmHTML(pk, name, host, icon, _status, fav, is_last) {
   return '<a href="/dashboard/vm/' + pk + '/" class="list-group-item' +
          (is_last ? ' list-group-item-last' : '') + '">' +      
         '<span class="index-vm-list-name">' + 
-          '<i class="' + icon + '" title="' + _status + '"></i> ' + name +
+          '<i class="fa ' + icon + '" title="' + _status + '"></i> ' + name +
         '</span>' + 
         '<small class="text-muted"> ' + host + '</small>' +
         '<div class="pull-right dashboard-vm-favourite" data-vm="' + pk + '">' +  
@@ -401,7 +401,7 @@ function generateGroupHTML(url, name) {
 function generateNodeHTML(name, icon, _status, url, is_last) {
   return '<a href="' + url + '" class="list-group-item real-link' + (is_last ? ' list-group-item-last' : '') + '">' +
         '<span class="index-node-list-name">' +
-        '<i class="' + icon + '" title="' + _status + '"></i> ' + name +
+        '<i class="fa ' + icon + '" title="' + _status + '"></i> ' + name +
         '</span>' +
         '<div style="clear: both;"></div>' +
         '</a>';
