d5c535da39f4709aee77b722b418f8b7485fc219
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-07-23 09:48:59 +0200

    dashboard: add exception handling

--- circle/dashboard/virtvalidator.py
+++ circle/dashboard/virtvalidator.py
@@ -1,20 +1,31 @@
 from django.core.exceptions import ValidationError
 from lxml import etree as ET
-
+import logging
 
 rng_file = "/usr/share/libvirt/schemas/domain.rng"
 
-header = """<domain type='kvm'>
-  <name>validator</name>
-  <memory unit='KiB'>1024</memory>
-  <os>
-    <type>hvm</type>
-  </os>"""
-footer = """</domain>"""
+# Mandatory xml elements dor parsing
+header = "<domain type='kvm'><name>validator</name>\
+<memory unit='KiB'>1024</memory>\
+<os><type>hvm</type></os>"
+footer = "</domain>"
+
+logger = logging.getLogger()
 
 
 def domain_validator(value):
     xml = header + value + footer
-    relaxng = ET.RelaxNG(file=rng_file)
-    if not relaxng.validate(ET.fromstring(xml)):
-        raise ValidationError("%s is not valid libvirt Domain xml." % value)
+    try:
+        parsed_xml = ET.fromstring(xml)
+    except Exception as e:
+        raise ValidationError(e.message)
+    try:
+        relaxng = ET.RelaxNG(file=rng_file)
+    except:
+        logger.critical("%s RelaxNG libvirt domain schema file "
+                        "is missing for validation.", rng_file)
+    else:
+        try:
+            relaxng.assertValid(parsed_xml)
+        except Exception as e:
+            raise ValidationError(e.message)
