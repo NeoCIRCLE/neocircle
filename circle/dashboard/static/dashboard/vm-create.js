e4088f016ea253a1df8ab3e9f76da7da657d9919
Kálmán Viktor <kviktor@cloud.bme.hu>
2014-07-31 14:57:09 +0200

    dashboard: clean up vm create

--- circle/dashboard/static/dashboard/vm-create.js
+++ circle/dashboard/static/dashboard/vm-create.js
@@ -64,6 +64,14 @@ function vmCreateLoaded() {
     return false;
   });
 
+  $('.progress-bar').each(function() {
+    var min = $(this).attr('aria-valuemin');
+    var max = $(this).attr('aria-valuemax');
+    var now = $(this).attr('aria-valuenow');
+    var siz = (now-min)*100/(max-min);
+    $(this).css('width', siz+'%');
+  }); 
+
 }
 
 function vmCustomizeLoaded() {
