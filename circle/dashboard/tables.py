4ae3498478dac3bf4d14005bb5571bd4b9fbf257
Kálmán Viktor <kviktor@cloud.bme.hu>
2014-08-06 19:26:19 +0200

    dashboard: clean up node detail

--- circle/dashboard/tables.py
+++ circle/dashboard/tables.py
@@ -22,7 +22,7 @@ from django_tables2 import Table, A
 from django_tables2.columns import (TemplateColumn, Column, BooleanColumn,
                                     LinkColumn)
 
-from vm.models import Instance, Node, InstanceTemplate, Lease
+from vm.models import Node, InstanceTemplate, Lease
 from django.utils.translation import ugettext_lazy as _
 from django_sshkey.models import UserKey
 
@@ -138,43 +138,6 @@ class UserListTable(Table):
         attrs = {'class': ('table table-bordered table-striped table-hover '
                            'vm-list-table')}
         fields = ('pk', 'username', )
-
-
-class NodeVmListTable(Table):
-    pk = TemplateColumn(
-        template_name='dashboard/vm-list/column-id.html',
-        verbose_name="ID",
-        attrs={'th': {'class': 'vm-list-table-thin'}},
-    )
-
-    name = TemplateColumn(
-        template_name="dashboard/vm-list/column-name.html"
-    )
-
-    admin = TemplateColumn(
-        template_name='dashboard/vm-list/column-admin.html',
-        attrs={'th': {'class': 'vm-list-table-admin'}},
-    )
-    details = TemplateColumn(
-        template_name='dashboard/vm-list/column-details.html',
-        attrs={'th': {'class': 'vm-list-table-thin'}},
-    )
-    actions = TemplateColumn(
-        template_name='dashboard/vm-list/column-actions.html',
-        attrs={'th': {'class': 'vm-list-table-thin'}},
-    )
-    time_of_suspend = TemplateColumn(
-        '{{ record.time_of_suspend|timeuntil }}',
-        verbose_name=_("Suspend in"))
-    time_of_delete = TemplateColumn(
-        '{{ record.time_of_delete|timeuntil }}',
-        verbose_name=_("Delete in"))
-
-    class Meta:
-        model = Instance
-        attrs = {'class': ('table table-bordered table-striped table-hover '
-                           'vm-list-table')}
-        fields = ('pk', 'name', 'state', 'time_of_suspend', 'time_of_delete', )
 
 
 class UserListTablex(Table):
