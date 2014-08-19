d2b495932674ebed67148ae806a0f7ac4fb0f8c2
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-08-19 11:17:36 +0200

    remove monitor tasks from mancelery

--- circle/manager/mancelery.py
+++ circle/manager/mancelery.py
@@ -31,7 +31,6 @@ celery = Celery('manager',
                          'storage.tasks.local_tasks',
                          'storage.tasks.periodic_tasks',
                          'firewall.tasks.local_tasks',
-                         'monitor.tasks.local_periodic_tasks',
                          'dashboard.tasks.local_periodic_tasks',
                          ])
 
@@ -42,20 +41,8 @@ celery.conf.update(
     CELERY_QUEUES=(
         Queue(HOSTNAME + '.man', Exchange('manager', type='direct'),
               routing_key="manager"),
-        Queue(HOSTNAME + '.monitor', Exchange('monitor', type='direct'),
-              routing_key="monitor"),
     ),
     CELERYBEAT_SCHEDULE={
-        'vm.update_domain_states': {
-            'task': 'vm.tasks.local_periodic_tasks.update_domain_states',
-            'schedule': timedelta(seconds=10),
-            'options': {'queue': 'localhost.man'}
-        },
-        'vm.garbage_collector': {
-            'task': 'vm.tasks.local_periodic_tasks.garbage_collector',
-            'schedule': timedelta(minutes=10),
-            'options': {'queue': 'localhost.man'}
-        },
         'storage.periodic_tasks': {
             'task': 'storage.tasks.periodic_tasks.garbage_collector',
             'schedule': timedelta(hours=1),
@@ -65,24 +52,6 @@ celery.conf.update(
             'task': 'dashboard.tasks.local_periodic_tasks.'
             'send_email_notifications',
             'schedule': timedelta(hours=24),
-            'options': {'queue': 'localhost.man'}
-        },
-        'monitor.measure_response_time': {
-            'task': 'monitor.tasks.local_periodic_tasks.'
-                    'measure_response_time',
-            'schedule': timedelta(seconds=30),
-            'options': {'queue': 'localhost.man'}
-        },
-        'monitor.check_celery_queues': {
-            'task': 'monitor.tasks.local_periodic_tasks.'
-                    'check_celery_queues',
-            'schedule': timedelta(seconds=60),
-            'options': {'queue': 'localhost.man'}
-        },
-        'monitor.instance_per_template': {
-            'task': 'monitor.tasks.local_periodic_tasks.'
-                    'instance_per_template',
-            'schedule': timedelta(seconds=30),
             'options': {'queue': 'localhost.man'}
         },
     }
