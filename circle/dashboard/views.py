53da6b24cecb13949e138402ebf0881c8bd85d6e
Őry Máté <ory.mate@cloud.bme.hu>
2014-08-25 09:55:11 +0200

    dashboard: fix method call

--- circle/dashboard/views.py
+++ circle/dashboard/views.py
@@ -1912,9 +1912,10 @@ class VmCreate(LoginRequiredMixin, TemplateView):
         if not template.has_level(request.user, 'user'):
             raise PermissionDenied()
 
-        name = request.POST.get("name", None)
-        instances = [Instance.create_from_template(
-            template=template, owner=user, name=name)]
+        args = {"template": template, "owner": user}
+        if "name" in request.POST:
+            args["name"] = request.POST.get("name")
+        instances = [Instance.create_from_template(**args)]
         return self.__deploy(request, instances)
 
     def __create_customized(self, request, *args, **kwargs):
