131d21d25de912b8b17bcd16fc959abdaf403066
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-09-23 14:15:57 +0200

    vm/models: rename resize_disk and fix disk size value

--- circle/vm/models/instance.py
+++ circle/vm/models/instance.py
@@ -790,15 +790,17 @@ class Instance(AclBase, VirtualMachineDescModel, StatusModel, OperatedMixin,
             else:
                 raise
 
-    def resize_disk(self, disk, size, timeout=15):
+    def resize_disk_live(self, disk, size, timeout=15):
         try:
             queue_name = self.get_remote_queue_name('vm', 'slow')
-            return vm_task.resize_disk.apply_async(
+            return vm_tasks.resize_disk.apply_async(
                 args=[self.vm_name, disk.path, size],
-                queue = queue_name).get(timeout=timeout)
+                queue=queue_name).get(timeout=timeout)
         except:
             raise
-
+        else:
+            disk.size = size
+            disk.save()
 
     def deploy_disks(self):
         """Deploy all associated disks.
