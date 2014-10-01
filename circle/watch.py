8d8e851e2d0d662df12ce21f3e5fb0ba85a6b152
Kálmán Viktor <kviktor@cloud.bme.hu>
2014-10-01 08:52:45 +0200

    circle: don't run watch.py on import

--- circle/watch.py
+++ circle/watch.py
@@ -27,41 +27,39 @@ class LessUtils(object):
                 less_pathname, css_pathname))
 
 
-# for first run compile everything
-print("Initial LESS compiles")
-current_dir = os.path.dirname(os.path.realpath(__file__))
+if __name__ == "__main__":
+    # for first run compile everything
+    print("Initial LESS compiles")
+    current_dir = os.path.dirname(os.path.realpath(__file__))
 
-for root, dirs, files in os.walk("/home/cloud/circle/circle"):
-    for f in files:
-        if not f.endswith(".less"):
-            continue
+    for root, dirs, files in os.walk("/home/cloud/circle/circle"):
+        for f in files:
+            if not f.endswith(".less"):
+                continue
 
-        relpath = os.path.relpath(root, current_dir)
-        if relpath.startswith(("static_collected", "bower_components")):
-            continue
+            relpath = os.path.relpath(root, current_dir)
+            if relpath.startswith(("static_collected", "bower_components")):
+                continue
 
-        less_pathname = "%s/%s" % (root, f)
-        css_pathname = LessUtils.less_path_to_css_path(less_pathname)
-        LessUtils.compile_less(less_pathname, css_pathname)
+            less_pathname = "%s/%s" % (root, f)
+            css_pathname = LessUtils.less_path_to_css_path(less_pathname)
+            LessUtils.compile_less(less_pathname, css_pathname)
 
-print("\n%s\n" % ("=" * 30))
-print("End of initial LESS compiles\n")
+    print("\n%s\n" % ("=" * 30))
+    print("End of initial LESS compiles\n")
 
+    # after first run watch less files
+    wm = pyinotify.WatchManager()
 
-# after first run watch less files
-wm = pyinotify.WatchManager()
+    class EventHandler(pyinotify.ProcessEvent):
+        def process_IN_MODIFY(self, event):
+            if not event.name.endswith(".less"):
+                return
 
+            css_pathname = LessUtils.less_path_to_css_path(event.pathname)
+            LessUtils.compile_less(event.pathname, css_pathname)
 
-class EventHandler(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(self, event):
-        if not event.name.endswith(".less"):
-            return
-
-        css_pathname = LessUtils.less_path_to_css_path(event.pathname)
-        LessUtils.compile_less(event.pathname, css_pathname)
-
-
-handler = EventHandler()
-notifier = pyinotify.Notifier(wm, handler)
-wm.add_watch(current_dir, pyinotify.IN_MODIFY, rec=True)
-notifier.loop()
+    handler = EventHandler()
+    notifier = pyinotify.Notifier(wm, handler)
+    wm.add_watch(current_dir, pyinotify.IN_MODIFY, rec=True)
+    notifier.loop()
