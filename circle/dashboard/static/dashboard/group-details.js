bb4dfc17d6ce30c9d2aa8969fe44ad46eb42bdaa
Kálmán Viktor <kviktor@cloud.bme.hu>
2015-01-16 13:13:22 +0100

    dashboard: group rename without js

--- circle/dashboard/static/dashboard/group-details.js
+++ circle/dashboard/static/dashboard/group-details.js
@@ -1,30 +1,31 @@
+$(function() {
   /* rename */
   $("#group-details-h1-name, .group-details-rename-button").click(function() {
-    $("#group-details-h1-name").hide();
-    $("#group-details-rename").css('display', 'inline');
-    $("#group-details-rename-name").focus();
+    $("#group-details-h1-name span").hide();
+    $("#group-details-rename-form").show().css('display', 'inline-block');
+    $("#group-details-rename-name").select();
   });
 
   /* rename ajax */
   $('#group-details-rename-submit').click(function() {
+    if(!$("#group-details-rename-name")[0].checkValidity()) {
+      return true;
+    }
     var name = $('#group-details-rename-name').val();
+
     $.ajax({
       method: 'POST',
       url: location.href,
       data: {'new_name': name},
       headers: {"X-CSRFToken": getCookie('csrftoken')},
       success: function(data, textStatus, xhr) {
-        $("#group-details-h1-name").text(data['new_name']).show();
-        $('#group-details-rename').hide();
-        // addMessage(data['message'], "success");
+        $("#group-details-h1-name span").text(data.new_name).show();
+        $('#group-details-rename-form').hide();
       },
       error: function(xhr, textStatus, error) {
-        addMessage("Error during renaming!", "danger");
+        addMessage("Error during renaming.", "danger");
       }
     });
     return false;
   });
-
-  $(".group-details-help-button").click(function() {
-    $(".group-details-help").stop().slideToggle();
-  });
+});
