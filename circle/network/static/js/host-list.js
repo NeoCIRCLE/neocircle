0729a05cff1572c5104c49b921707f72370967a9
Kálmán Viktor <kviktor@cloud.bme.hu>
2015-01-21 12:26:12 +0100

    network: fix jshint warnings

--- circle/network/static/js/host-list.js
+++ circle/network/static/js/host-list.js
@@ -1,6 +1,6 @@
 $(function() {
   if($("#network-host-list-table").length) {
-    var order = ["hostname", "vlan", "mac", "ipv4", "ipv6", "external_ipv4", "created_at", "owner"]
+    var order = ["hostname", "vlan", "mac", "ipv4", "ipv6", "external_ipv4", "created_at", "owner"];
     var options = {
       paging: false,
         columnDefs: [
@@ -10,7 +10,7 @@ $(function() {
         language: {
           zeroRecords: gettext("No host found.")
         }
-    }
+    };
     table = createDataTable($("#network-host-list-table"), options, "sort", order);
 
     $("#network-host-list-input").keyup(function() {
@@ -31,7 +31,7 @@ function createDataTable(table_element, options, sort_parameter_name, sort_order
   var dir = "asc";
   var index = 0;
   if(sort.length > 0 && sort[0] == "-") {
-    dir = "desc"
+    dir = "desc";
     sort = sort.substr(1, sort.length - 1);
   }
   if(sort)
