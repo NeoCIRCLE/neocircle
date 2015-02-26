5456e8b3d3d6b78596679ea83eada815c01e90a5
Bach Dániel <bd@ik.bme.hu>
2015-02-26 15:09:57 +0100

    circle: remove garbage

--- circle/circle/urls.py
+++ circle/circle/urls.py
@@ -32,13 +32,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # url(r'^$', TemplateView.as_view(template_name='base.html')),
-
-    # Examples:
-    # url(r'^$', 'circle.views.home', name='home'),
-    # url(r'^circle/', include('circle.foo.urls')),
-
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^$', lambda x: redirect(reverse("dashboard.index"))),
     url(r'^network/', include('network.urls')),
