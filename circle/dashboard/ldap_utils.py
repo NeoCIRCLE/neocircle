c18cc04227716d459cc3a0f9ed9fdae50a867d5e
Czémán Arnold <czeman.arnold@cloud.bme.hu>
2017-02-16 03:01:06 +0100

    dashboard: add license for ldap_utils.py

--- circle/dashboard/ldap_utils.py
+++ circle/dashboard/ldap_utils.py
@@ -1,3 +1,19 @@
+# Copyright 2014 Budapest University of Technology and Economics (BME IK)
+#
+# This file is part of CIRCLE Cloud.
+#
+# CIRCLE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# CIRCLE is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with CIRCLE.  If not, see <http://www.gnu.org/licenses/>.
 import logging
 from django.conf import settings
 from django.contrib.auth.models import Group
