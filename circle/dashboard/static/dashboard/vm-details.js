393afbe5db517433070b586295dfee440700cef2
Czémán Arnold <czeman.arnold@cloud.bme.hu>
2017-05-29 22:31:31 +0200

    Hide command strings, it solves issue #474

--- circle/dashboard/static/dashboard/vm-details.js
+++ circle/dashboard/static/dashboard/vm-details.js
@@ -70,12 +70,12 @@ $(function() {
   });
 
   /* for js fallback */
-  $("#vm-details-pw-show").parent("div").children("input").prop("type", "password");
+  $(".vm-details-show-password").parent("div").children("input").prop("type", "password");
 
   /* show password */
-  $("#vm-details-pw-show").click(function() {
+  $(".vm-details-show-password").click(function() {
     var input = $(this).parent("div").children("input");
-    var eye = $(this).children("#vm-details-pw-eye");
+    var eye = $(this).children(".vm-details-password-eye");
     var span = $(this);
 
     span.tooltip("destroy");
@@ -253,6 +253,11 @@ $(function() {
   });
 
   // Clipboard for connection strings
-  if(Clipboard.isSupported())
-    new Clipboard(".vm-details-connection-string-copy");
+  if(Clipboard.isSupported()) {
+    new Clipboard(".vm-details-connection-string-copy", {
+      text: function(trigger) {
+        return $($(trigger).data("clipboard-target")).val();
+      }
+    });
+  }
 });
